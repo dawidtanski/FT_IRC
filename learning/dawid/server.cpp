a619b8ef36fc8a996c3c86286bd5cf46d83b6269
Dawid <dawtan96@gmail.com>
2026-08-06 00:37:24 +0200

    added inet ntop2 function to support both ipv4 or ipv6

--- learning/dawid/server.cpp
+++ learning/dawid/server.cpp
@@ -8,8 +8,9 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <poll.h>
+#include <iostream>
 
-#define PORT 
+#define PORT 4242
 
 
 // Function to handle partial sending. Send might not send all the bytes we want to, so it's 
@@ -33,4 +34,34 @@ int sendall(int sock_fd, char *buf, int *len){
 
 	return n == -1 ? -1 : 0;
 
+}
+
+/*struct sockaddr_in6 {
+    uint16_t       sin6_family;   // AF_INET6
+    uint16_t       sin6_port;     // port
+    uint32_t       sin6_flowinfo; // informacje o przepływie
+    struct in6_addr sin6_addr;    // <-- ADRES IPv6
+    uint32_t       sin6_scope_id; // scope
+};*/
+
+//inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
+// Function that converts ip from "network to presentation"
+std::string inet_ntop2(const sockaddr_storage& addr){
+
+	char buf[INET6_ADDRSTRLEN];
+
+	if (addr.ss_family == AF_INET){
+		const sockaddr_in *sa4 = (const sockaddr_in *)&addr;
+		if (!inet_ntop(sa4->sin_family, &sa4->sin_addr, buf, INET6_ADDRSTRLEN))
+			throw std::runtime_error("inet_ntop failed");
+		return buf;
+	}
+	else if (addr.ss_family == AF_INET6){
+		const sockaddr_in6 *sa6 = (const sockaddr_in6 *)&addr;
+		if(!inet_ntop(sa6->sin6_family, &sa6->sin6_addr, buf, INET6_ADDRSTRLEN))
+			throw std::runtime_error("inet_ntop failed");
+		return buf;
+	}
+	else
+		throw std::runtime_error("Couldn't convert address from binary to string");
 }
